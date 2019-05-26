5966707e45603f41f6462b9933dac486ae1b01af
Ahmadreza Nani <ahmadr4079@gmail.com>
2019-05-26 19:51:55 +0430

    check if token csv file exist then remove and write genereate token file in function

--- csvTokenGenerate.py
+++ csvTokenGenerate.py
@@ -1,15 +1,33 @@
 import pandas as pd
 from uuid import uuid1
+import subprocess
+import os
 
 
-initialData = []
-keywordFile = open('cPlusKeyword.txt','r')
-for item in keywordFile:
-    initialData.append([uuid1(),'keyword',item])
-keywordFile.close()
+def generateTokenCsvFile():
+    process = subprocess.Popen("ls", stdout=subprocess.PIPE, shell=True)
+    (output,err) = process.communicate()
+    if 'token.csv' in output.decode('utf-8'):
+        os.remove('token.csv')
+        initialData = []
+        keywordFile = open('cPlusKeyword.txt','r')
+        for item in keywordFile:
+            initialData.append([uuid1(),'keyword',item])
+        keywordFile.close()
 
-dataFrameToken = pd.DataFrame(data=initialData,columns=['id','tokenName','attributeValue'])
-dataFrameToken.to_csv('token.csv')
+        dataFrameToken = pd.DataFrame(data=initialData,columns=['id','tokenName','attributeValue'])
+        dataFrameToken.to_csv('token.csv')
+        
+        return True
+    else:
+        initialData = []
+        keywordFile = open('cPlusKeyword.txt','r')
+        for item in keywordFile:
+            initialData.append([uuid1(),'keyword',item])
+        keywordFile.close()
 
+        dataFrameToken = pd.DataFrame(data=initialData,columns=['id','tokenName','attributeValue'])
+        dataFrameToken.to_csv('token.csv')
 
+        return True
 
