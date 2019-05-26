a4cc7e0d7268983fa5d0ce174f1b65caea3b112d
Ahmadreza Nani <ahmadr4079@gmail.com>
2019-05-26 21:47:16 +0430

    delete '\n' from end of attribute value of keyword token

--- csvTokenGenerate.py
+++ csvTokenGenerate.py
@@ -12,7 +12,7 @@ def generateTokenCsvFile():
         initialData = []
         keywordFile = open('cPlusKeyword.txt','r')
         for item in keywordFile:
-            initialData.append([uuid1(),'keyword',item])
+            initialData.append([uuid1(),'keyword',item.split('\n')[0]])
         keywordFile.close()
 
         dataFrameToken = pd.DataFrame(data=initialData,columns=['id','tokenName','attributeValue'])
@@ -23,7 +23,7 @@ def generateTokenCsvFile():
         initialData = []
         keywordFile = open('cPlusKeyword.txt','r')
         for item in keywordFile:
-            initialData.append([uuid1(),'keyword',item])
+            initialData.append([uuid1(),'keyword',item.split('\n')[0]])
         keywordFile.close()
 
         dataFrameToken = pd.DataFrame(data=initialData,columns=['id','tokenName','attributeValue'])
