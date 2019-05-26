25fc50efd0bb659739160eae64d7bca3af4cfe62
Ahmadreza Nani <ahmadr4079@gmail.com>
2019-05-26 19:26:23 +0430

    store c++ keyword from file to token csv file

--- csvTokenGenerate.py
+++ csvTokenGenerate.py
@@ -2,9 +2,14 @@ import pandas as pd
 from uuid import uuid1
 
 
-initialData = [[uuid1(),'keyword','float']]
+initialData = []
+keywordFile = open('cPlusKeyword.txt','r')
+for item in keywordFile:
+    initialData.append([uuid1(),'keyword',item])
+keywordFile.close()
+
 dataFrameToken = pd.DataFrame(data=initialData,columns=['id','tokenName','attributeValue'])
-dataFrameToken.to_csv('Token.csv')
+dataFrameToken.to_csv('token.csv')
 
 
 
