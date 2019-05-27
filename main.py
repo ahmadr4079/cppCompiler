b0cbb3487c9299fe3f140f1945bb118ec7e7b10e
Ahmadreza Nani <ahmadr4079@gmail.com>
2019-05-27 19:24:43 +0430

    use argument to get the code file

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fileScroller import FileScroller
 from lexicalAnalysis import LexicalAnalysis
 from csvTokenGenerate import generateTokenCsvFile
+import sys
 
 generateTokenCsvFile()
 
-file = FileScroller('mainCode.txt')
+file = FileScroller('{}'.format(sys.argv[1]))
 x = LexicalAnalysis(file)
 x.switchState(0)
 for item in x.tokens:
