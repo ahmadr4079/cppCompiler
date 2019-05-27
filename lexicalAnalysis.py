ed22388390b9b8d5f52539950b8b0bd58372cfb5
Ahmadreza Nani <ahmadr4079@gmail.com>
2019-05-27 10:26:39 +0430

    check state until 10 in re-numbering state

--- lexicalAnalysis.py
+++ lexicalAnalysis.py
@@ -20,21 +20,21 @@ class LexicalAnalysis:
         elif char == '=':
             return LexicalAnalysis.switchState(self, 5)
         elif char == '>':
-            return LexicalAnalysis.switchState(self, 6)
-        elif char == ' ':
-            return LexicalAnalysis.switchState(self, 22)
-        elif char == ';':
-            return LexicalAnalysis.switchState(self, 25)
-        elif char == '(':
-            return LexicalAnalysis.switchState(self, 26)
-        elif char == ')':
-            return LexicalAnalysis.switchState(self, 27)
-        elif char == '{':
-            return LexicalAnalysis.switchState(self, 28)
-        elif char == '}':
-            return LexicalAnalysis.switchState(self, 29)
-        elif re.search('[A-Z]|[a-z]', char):
-            return LexicalAnalysis.switchState(self, 9, char)
+            return LexicalAnalysis.switchState(self, 8)
+        # elif char == ' ':
+        #     return LexicalAnalysis.switchState(self, 22)
+        # elif char == ';':
+        #     return LexicalAnalysis.switchState(self, 25)
+        # elif char == '(':
+        #     return LexicalAnalysis.switchState(self, 26)
+        # elif char == ')':
+        #     return LexicalAnalysis.switchState(self, 27)
+        # elif char == '{':
+        #     return LexicalAnalysis.switchState(self, 28)
+        # elif char == '}':
+        #     return LexicalAnalysis.switchState(self, 29)
+        # elif re.search('[A-Z]|[a-z]', char):
+        #     return LexicalAnalysis.switchState(self, 9, char)
 
     def state_1(self):
         char = self.fp.nextChar()
@@ -67,147 +67,153 @@ class LexicalAnalysis:
             print("'<operator,EQ>'")
             print("'<eof>'")
         elif char == '=':
-            return LexicalAnalysis.switchState(self, 10)
+            return LexicalAnalysis.switchState(self, 6)
         else:
-            return LexicalAnalysis.switchState(self, 11)
+            return LexicalAnalysis.switchState(self, 7)
 
-    def state_10(self):
+    def state_6(self):
         print("'<operator,ET>'")
         return LexicalAnalysis.switchState(self, 0)
 
-    def state_11(self):
+    def state_7(self):
         self.fp.previousChar()
         print("'<operator,EQ>'")
         return LexicalAnalysis.switchState(self, 0)
 
-    def state_6(self):
+    def state_8(self):
         char = self.fp.nextChar()
         if char == 'eof':
             print("'<operator,GT>'")
             print("'<eof>'")
         elif char == '=':
-            return LexicalAnalysis.switchState(self, 7)
+            return LexicalAnalysis.switchState(self, 9)
         else:
-            return LexicalAnalysis.switchState(self, 8)
+            return LexicalAnalysis.switchState(self, 10)
 
-    def state_7(self):
+    def state_9(self):
         print("'<operator,GE>''")
         return LexicalAnalysis.switchState(self, 0)
 
-    def state_8(self):
+    def state_10(self):
         self.fp.previousChar()
         print("'<operator,GT>'")
         return LexicalAnalysis.switchState(self, 0)
 
-    def checkToken(self):
-        string = self.identifiers
-        tokenDataFrame = pd.read_csv('token.csv', index_col=0)
-        for item, value in tokenDataFrame.iterrows():
-            if value['tokenName'] == 'keyword' and value['attributeValue'] == string:
-                return value['id'], value['tokenName'], value['attributeValue']
-        return None, None, None
 
-    def addIdentifiers(self):
-        string = self.identifiers
-        tokenDataFrame = pd.read_csv('token.csv', index_col=0)
-        tokenDataFrame = tokenDataFrame.append({'id': uuid1(),
-                                                'tokenName': 'identifier',
-                                                'attributeValue': string},
-                                               ignore_index=True)
-        tokenDataFrame.to_csv('token.csv')
-        return True
+    
 
-    def state_9(self, *argv):
-        if argv:
-            self.identifiers = argv[0]
-        char = self.fp.nextChar()
-        if char == 'eof':
-            tokenId, tokenName, tokenAttributeValue = LexicalAnalysis.checkToken(
-                self)
-            if tokenId:
-                print("'<{},{}>'".format(tokenName, tokenAttributeValue))
-            else:
-                addTokenBool = LexicalAnalysis.addIdentifiers(self)
-                if addTokenBool:
-                    print("'<identifiers,{}>'".format(self.identifiers))
-                else:
-                    print('error to save token in token csv file')
-            print("'<eof>'")
-        elif re.search('[A-Z]|[a-z]|[0-9]', char):
-            self.identifiers = self.identifiers + char
-            return LexicalAnalysis.switchState(self, 9)
-        else:
-            self.fp.previousChar()
-            tokenId, tokenName, tokenAttributeValue = LexicalAnalysis.checkToken(
-                self)
-            if tokenId:
-                print("'<{},{}>'".format(tokenName, tokenAttributeValue))
-            else:
-                addTokenBool = LexicalAnalysis.addIdentifiers(self)
-                if addTokenBool:
-                    print("'<identifiers,{}>'".format(self.identifiers))
-                else:
-                    print('error to save token in token csv file')
-            return LexicalAnalysis.switchState(self, 0)
 
-    def state_22(self):
-        char = self.fp.nextChar()
-        if char == 'eof':
-            print("'<delim>'")
-            print("'<eof>'")
-        elif char == ' ':
-            return LexicalAnalysis.switchState(self, 22)
-        else:
-            print("'<delim>'")
-            self.fp.previousChar()
-            return LexicalAnalysis.switchState(self, 0)
 
-    def state_25(self):
-        char = self.fp.nextChar()
-        if char == 'eof':
-            print("'<punctuation,semicolon>'")
-            print("'<eof>'")
-        else:
-            print("'<punctuation,semicolon>'")
-            self.fp.previousChar()
-            return LexicalAnalysis.switchState(self, 0)
 
-    def state_26(self):
-        char = self.fp.nextChar()
-        if char == 'eof':
-            print("'<punctuation,leftparantheses>'")
-            print("'<eof>'")
-        else:
-            print("'<punctuation,leftparantheses>'")
-            self.fp.previousChar()
-            return LexicalAnalysis.switchState(self, 0)
+    # def checkToken(self):
+    #     string = self.identifiers
+    #     tokenDataFrame = pd.read_csv('token.csv', index_col=0)
+    #     for item, value in tokenDataFrame.iterrows():
+    #         if value['tokenName'] == 'keyword' and value['attributeValue'] == string:
+    #             return value['id'], value['tokenName'], value['attributeValue']
+    #     return None, None, None
 
-    def state_27(self):
-        char = self.fp.nextChar()
-        if char == 'eof':
-            print("'<punctuation,rightparantheses>'")
-            print("'<eof>'")
-        else:
-            print("'<punctuation,rightparantheses>'")
-            self.fp.previousChar()
-            return LexicalAnalysis.switchState(self, 0)
+    # def addIdentifiers(self):
+    #     string = self.identifiers
+    #     tokenDataFrame = pd.read_csv('token.csv', index_col=0)
+    #     tokenDataFrame = tokenDataFrame.append({'id': uuid1(),
+    #                                             'tokenName': 'identifier',
+    #                                             'attributeValue': string},
+    #                                            ignore_index=True)
+    #     tokenDataFrame.to_csv('token.csv')
+    #     return True
 
-    def state_28(self):
-        char = self.fp.nextChar()
-        if char == 'eof':
-            print("'<punctuation,leftbracket>'")
-            print("'<eof>'")
-        else:
-            print("'<punctuation,leftbracket>'")
-            self.fp.previousChar()
-            return LexicalAnalysis.switchState(self, 0)
+    # def state_9(self, *argv):
+    #     if argv:
+    #         self.identifiers = argv[0]
+    #     char = self.fp.nextChar()
+    #     if char == 'eof':
+    #         tokenId, tokenName, tokenAttributeValue = LexicalAnalysis.checkToken(
+    #             self)
+    #         if tokenId:
+    #             print("'<{},{}>'".format(tokenName, tokenAttributeValue))
+    #         else:
+    #             addTokenBool = LexicalAnalysis.addIdentifiers(self)
+    #             if addTokenBool:
+    #                 print("'<identifiers,{}>'".format(self.identifiers))
+    #             else:
+    #                 print('error to save token in token csv file')
+    #         print("'<eof>'")
+    #     elif re.search('[A-Z]|[a-z]|[0-9]', char):
+    #         self.identifiers = self.identifiers + char
+    #         return LexicalAnalysis.switchState(self, 9)
+    #     else:
+    #         self.fp.previousChar()
+    #         tokenId, tokenName, tokenAttributeValue = LexicalAnalysis.checkToken(
+    #             self)
+    #         if tokenId:
+    #             print("'<{},{}>'".format(tokenName, tokenAttributeValue))
+    #         else:
+    #             addTokenBool = LexicalAnalysis.addIdentifiers(self)
+    #             if addTokenBool:
+    #                 print("'<identifiers,{}>'".format(self.identifiers))
+    #             else:
+    #                 print('error to save token in token csv file')
+    #         return LexicalAnalysis.switchState(self, 0)
 
-    def state_29(self):
-        char = self.fp.nextChar()
-        if char == 'eof':
-            print("'<punctuation,rightbracket>'")
-            print("'<eof>'")
-        else:
-            print("'<punctuation,rightbracket>'")
-            self.fp.previousChar()
-            return LexicalAnalysis.switchState(self, 0)
+    # def state_22(self):
+    #     char = self.fp.nextChar()
+    #     if char == 'eof':
+    #         print("'<delim>'")
+    #         print("'<eof>'")
+    #     elif char == ' ':
+    #         return LexicalAnalysis.switchState(self, 22)
+    #     else:
+    #         print("'<delim>'")
+    #         self.fp.previousChar()
+    #         return LexicalAnalysis.switchState(self, 0)
+
+    # def state_25(self):
+    #     char = self.fp.nextChar()
+    #     if char == 'eof':
+    #         print("'<punctuation,semicolon>'")
+    #         print("'<eof>'")
+    #     else:
+    #         print("'<punctuation,semicolon>'")
+    #         self.fp.previousChar()
+    #         return LexicalAnalysis.switchState(self, 0)
+
+    # def state_26(self):
+    #     char = self.fp.nextChar()
+    #     if char == 'eof':
+    #         print("'<punctuation,leftparantheses>'")
+    #         print("'<eof>'")
+    #     else:
+    #         print("'<punctuation,leftparantheses>'")
+    #         self.fp.previousChar()
+    #         return LexicalAnalysis.switchState(self, 0)
+
+    # def state_27(self):
+    #     char = self.fp.nextChar()
+    #     if char == 'eof':
+    #         print("'<punctuation,rightparantheses>'")
+    #         print("'<eof>'")
+    #     else:
+    #         print("'<punctuation,rightparantheses>'")
+    #         self.fp.previousChar()
+    #         return LexicalAnalysis.switchState(self, 0)
+
+    # def state_28(self):
+    #     char = self.fp.nextChar()
+    #     if char == 'eof':
+    #         print("'<punctuation,leftbracket>'")
+    #         print("'<eof>'")
+    #     else:
+    #         print("'<punctuation,leftbracket>'")
+    #         self.fp.previousChar()
+    #         return LexicalAnalysis.switchState(self, 0)
+
+    # def state_29(self):
+    #     char = self.fp.nextChar()
+    #     if char == 'eof':
+    #         print("'<punctuation,rightbracket>'")
+    #         print("'<eof>'")
+    #     else:
+    #         print("'<punctuation,rightbracket>'")
+    #         self.fp.previousChar()
+    #         return LexicalAnalysis.switchState(self, 0)
