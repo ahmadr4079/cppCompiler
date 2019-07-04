fa226a203a343cfaecd617f1792c8a0854ba9a89
Ahmadreza Nani <ahmadr4079@gmail.com>
2019-07-04 10:17:02 +0430

    manage nextToken in tokenList class

--- tokenList.py
+++ tokenList.py
@@ -2,7 +2,14 @@ class TokenList:
 
     def __init__(self,list):
         self.list = list
+        self.pointer = -1
         
     def printTokenList(self):
         for item in self.list:
             print(item)
+
+    def nextToken(self):
+        self.pointer = self.pointer + 1
+        self.token = self.list[self.pointer]
+        return self.token
+        
