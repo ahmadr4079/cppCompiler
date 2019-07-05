26dcf30d463449c8d824f88707488103da8c24be
Ahmadreza Nani <ahmadr4079@gmail.com>
2019-07-05 16:07:18 +0430

    handle index of token list out of range error

--- tokenList.py
+++ tokenList.py
@@ -3,13 +3,14 @@ class TokenList:
     def __init__(self,list):
         self.list = list
         self.pointer = -1
-        
+        self.maxListIndex = len(list)
+
     def printTokenList(self):
         for item in self.list:
             print(item)
 
     def nextToken(self):
         self.pointer = self.pointer + 1
-        self.token = self.list[self.pointer]
-        return self.token
-        
+        if(self.pointer < self.maxListIndex):
+            self.token = self.list[self.pointer]
+            return self.token
