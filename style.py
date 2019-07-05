0b4534999cf85669a12916fb14e7a7a7923508d8
Ahmadreza Nani <ahmadr4079@gmail.com>
2019-07-05 14:23:30 +0430

    added style class for customize color of str in print

---
+++ style.py
@@ -0,0 +1,11 @@
+class Style():
+    black = lambda x: '\033[30m' + str(x)
+    red = lambda x: '\033[31m' + str(x)
+    green = lambda x: '\033[32m' + str(x)
+    yellow = lambda x: '\033[33m' + str(x)
+    blue = lambda x: '\033[34m' + str(x)
+    magenta = lambda x: '\033[35m' + str(x)
+    cyan = lambda x: '\033[36m' + str(x)
+    white = lambda x: '\033[37m' + str(x)
+    underline = lambda x: '\033[4m' + str(x)
+    reset = lambda x: '\033[0m' + str(x)
